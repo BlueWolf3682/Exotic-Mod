8c055aece5596b29ebf77bbb5d7090c6e8179a88
BlueWolf <61173114+BlueWolf3682@users.noreply.github.com>
2021-01-22 22:23:56 -0700

    add battleground

--- scripts/planets/sectorPresets.js
+++ scripts/planets/sectorPresets.js
@@ -4,4 +4,10 @@ const lastStand = new SectorPreset("last-stand", wandura, 69);
 lastStand.localizedName = "Last Stand";
 lastStand.difficulty = 25;
 lastStand.captureWave = 150;
-lastStand.alwaysUnlocked = true;
+lastStand.alwaysUnlocked = false;
+
+const overgrownBattleground = new SectorPreset("overgrown-battleground", wandura, 41);
+overgrownBattleground.localizedName = "Overgrown Battleground";
+overgrownBattleground.difficulty = 6;
+overgrownBattleground.captureWave = 25;
+overgrownBattleground.alwaysUnlocked = true;
